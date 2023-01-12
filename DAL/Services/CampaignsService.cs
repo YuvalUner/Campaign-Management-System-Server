4419a3c2c4d6f90ce53230d45246bf80d9eae34b
Yuval Uner <yuvaluner@gmail.com>
2023-01-12 21:19:23 +0200

    [CMP-19][CMP-92] Began working on server functionality for adding a campaign.

---
+++ DAL/Services/CampaignsService.cs
@@ -0,0 +1,15 @@
+﻿using DAL.DbAccess;
+
+namespace DAL.Services;
+
+public class CampaignsService
+{
+    private readonly IGenericDbAccess _dbAccess;
+    
+    public CampaignsService(IGenericDbAccess dbAccess)
+    {
+        _dbAccess = dbAccess;
+    }
+    
+    
+}
