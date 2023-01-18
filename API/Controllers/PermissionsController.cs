6dead39233155701522bea8ac404a581c51dd70b
Yuval Uner <yuvaluner@gmail.com>
2023-01-18 20:34:10 +0200

    [CMP-38][CMP-116] Added functionality for user to add permissions to other users.

---
+++ API/Controllers/PermissionsController.cs
@@ -0,0 +1,52 @@
+﻿using API.Utils;
+using DAL.Models;
+using DAL.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class PermissionsController: Controller
+{
+    private readonly IPermissionsService _permissionService;
+    private readonly ILogger<PermissionsController> _logger;
+    private readonly IUsersService _usersService;
+    
+    public PermissionsController(IPermissionsService permissionService, ILogger<PermissionsController> logger,
+        IUsersService usersService)
+    {
+        _permissionService = permissionService;
+        _logger = logger;
+        _usersService = usersService;
+    }
+    
+    [HttpPost("add/{userEmail}/{campaignGuid:guid}")]
+    public async Task<IActionResult> AddPermission(string userEmail, Guid campaignGuid, [FromBody] Permission permission)
+    {
+        try
+        {
+            if (!CampaignAuthorizationUtils.IsUserAuthorizedForCampaign(HttpContext, campaignGuid)
+                || !CampaignAuthorizationUtils.DoesActiveCampaignMatch(HttpContext, campaignGuid))
+            {
+                return Unauthorized();
+            }
+            // TODO: Check if user has permission to edit permissions
+            // TODO: Check if user can edit this permission
+            User? user = await _usersService.GetUserByEmail(userEmail);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            await _permissionService.AddPermission(permission, user.UserId, campaignGuid);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while adding permission");
+            return StatusCode(500);
+        }
+    }
+}
---
+++ DAL/Models/Permission.cs
@@ -0,0 +1,84 @@
+﻿namespace DAL.Models;
+
+public static class PermissionTypes
+{
+    public static readonly string View = "view";
+    public static readonly string Edit = "edit";
+
+    public static readonly string[] All = new string[] { View, Edit };
+
+    public static bool IsValid(string permissionType)
+    {
+        return All.Contains(permissionType);
+    }
+}
+
+public static class Screens
+{
+    public static readonly string CampaignSettings = "Campaign Settings";
+    public static readonly string Permissions = "Permissions";
+
+    public static readonly string[] All = new string[] { CampaignSettings, Permissions };
+
+    public static bool IsValid(string screen)
+    {
+        return All.Contains(screen);
+    }
+}
+
+public class Permission : IEquatable<Permission>
+{
+    public int? PermissionId { get; set; }
+
+    private string? _permissionType;
+
+    public string? PermissionType
+    {
+        get { return _permissionType; }
+        set
+        {
+            if (!PermissionTypes.IsValid(value))
+            {
+                throw new ArgumentException("Invalid permission type");
+            }
+
+            _permissionType = value;
+        }
+    }
+
+    private string? _screen;
+
+    public string? PermissionForScreen
+    {
+        get { return _screen; }
+        set
+        {
+            if (!Screens.IsValid(value))
+            {
+                throw new ArgumentException("Invalid screen");
+            }
+
+            _screen = value;
+        }
+    }
+
+    public bool Equals(Permission? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _permissionType == other._permissionType && PermissionForScreen == other.PermissionForScreen;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Permission)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_permissionType, PermissionForScreen);
+    }
+}
