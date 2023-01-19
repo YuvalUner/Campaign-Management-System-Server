27b8cdd7d4285ee0a8285e840d416bf56b52dfe6
Yuval Uner <yuvaluner@gmail.com>
2023-01-19 16:47:04 +0200

    [CMP-29][CMP-121][CMP-122] Added permission system and added preventions for all existing methods that require them, such that users can only access what they have permissions for.

---
+++ API/Utils/RoleUtils.cs
@@ -0,0 +1,20 @@
+﻿using API.SessionExtensions;
+using DAL.Models;
+using DAL.Services.Interfaces;
+
+namespace API.Utils;
+
+public static class RoleUtils
+{
+    public static async Task SetRole(IRolesService rolesService, HttpContext context)
+    {
+        var userId = context.Session.GetInt32(Constants.UserId);
+        var campaignGuid = context.Session.Get<Guid?>(Constants.ActiveCampaign);
+        
+        if (userId.HasValue && campaignGuid.HasValue)
+        {
+            var role = await rolesService.GetRoleInCampaign(campaignGuid.Value, userId);
+            context.Session.Set(Constants.ActiveCampaignRole, role);
+        }
+    }
+}
---
+++ DAL/Services/Interfaces/IRolesService.cs
@@ -0,0 +1,8 @@
+﻿using DAL.Models;
+
+namespace DAL.Services.Interfaces;
+
+public interface IRolesService
+{
+    Task<Role?> GetRoleInCampaign(Guid? campaignGuid, int? userId);
+}
