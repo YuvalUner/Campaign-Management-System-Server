63c95a1f0c76961207618cc5266cd2871e1e043d
Yuval Uner <yuvaluner@gmail.com>
2023-01-22 15:58:59 +0200

    [CMP-21] Added services for sending SMS and Email messages

---
+++ RestAPIServices/IEmailSendingService.cs
@@ -0,0 +1,7 @@
+﻿namespace RestAPIServices;
+
+public interface IEmailSendingService
+{
+    Task SendEmailAsync(string emailTo, string subject, string message);
+    Task SendUserJoinedEmailAsync(string? userName, string? campaignName, string emailTo);
+}
---
+++ RestAPIServices/SmsMessageSendingService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Telesign;
+
+namespace RestAPIServices;
+
+public interface ISmsMessageSendingService
+{
+    Task SendSmsMessageAsync(string phoneNumber, string message);
+    Task SendUserJoinedSmsAsync(string? userName, string? campaignName, string phoneNumber);
+}
+
+public class SmsMessageSendingService : ISmsMessageSendingService
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<SmsMessageSendingService> _logger;
+    
+    public SmsMessageSendingService(IConfiguration configuration, ILogger<SmsMessageSendingService> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task SendSmsMessageAsync(string phoneNumber, string message)
+    {
+        try
+        {
+            MessagingClient messagingClient = new MessagingClient(_configuration["SmsConfiguration:Telesign:CustomerId"],
+                _configuration["SmsConfiguration:Telesign:ApiKey"]);
+            RestClient.TelesignResponse telesignResponse = await messagingClient.MessageAsync(phoneNumber, message, "ARN");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while sending sms message");
+        }
+    }
+
+    public async Task SendUserJoinedSmsAsync(string? userName, string? campaignName, string phoneNumber)
+    {
+        string message = $"User {userName} joined campaign {campaignName}";
+        await SendSmsMessageAsync(phoneNumber, message);
+        
+    }
+}
