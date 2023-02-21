0b185b3e84e0067b92bf22612c4f09e5e33811c7
Yuval Uner <yuvaluner@gmail.com>
2023-02-21 16:09:18 +0200

    [CMP-31] Added DB functionality create, update and delete and some of the needed gets and additional creates for events.

---
+++ DAL/Models/GetUserEventsResult.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL.Models;
+
+public class GetUserEventsResult
+{
+    public Guid EventGuid { get; set; }
+    public string? EventName { get; set; }
+    public string? EventDescription { get; set; }
+    public string? EventLocation { get; set; }
+    public DateTime? EventStartTime { get; set; }
+    public DateTime? EventEndTime { get; set; }
+    public int? MaxAttendees { get; set; }
+    public Guid? CampaignGuid { get; set; }
+    public string? CampaignName { get; set; }
+    public string? CampaignLogoUrl { get; set; }
+}
