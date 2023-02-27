5767c54fb8dff55cfcf43faa4064d0c539d294a4
Yuval Uner <yuvaluner@gmail.com>
2023-02-27 16:52:19 +0200

    [CMP-32] Fixed up some methods in ScheduleManagersService.cs to also return status codes, added some basic tests to the service.

--- DAL/Services/Interfaces/IScheduleManagersService.cs
+++ DAL/Services/Interfaces/IScheduleManagersService.cs
@@ -5,7 +5,35 @@ namespace DAL.Services.Interfaces;
 
 public interface IScheduleManagersService
 {
-    Task<IEnumerable<User>> GetScheduleManagers(string userEmail);
+    /// <summary>
+    /// Gets the list of users who can manage the schedule of the given user.
+    /// </summary>
+    /// <param name="userEmail">Email of the user to get schedule managers for. This or userId must be provided.</param>
+    /// <param name="userId">User Id of the user to get schedule managers for. This or userEmail must be provided.</param>
+    /// <returns>Stored procedure's status code as item 1, list of schedule managers as item 2.</returns>
+    Task<(CustomStatusCode, IEnumerable<User>)> GetScheduleManagers(string? userEmail = null, int? userId = null);
+    
+    /// <summary>
+    /// Adds a schedule manager to the given user.
+    /// </summary>
+    /// <param name="giverUserId">The user id of the user that gives the schedule management permission</param>
+    /// <param name="receiverEmail">The email of the user that receives the schedule management permission</param>
+    /// <returns>Status code UserNotFound if the permission receiver does not exist,
+    /// DuplicateKey if user is already a schedule manage of the requesting user</returns>
     Task<CustomStatusCode> AddScheduleManager(int giverUserId, string receiverEmail);
+    
+    /// <summary>
+    /// Removes a schedule manager from the given user.
+    /// </summary>
+    /// <param name="giverUserId">The user id of the user that gave the schedule management permission.</param>
+    /// <param name="receiverEmail">The user id of the user that has the schedule management permission.</param>
+    /// <returns>Status code UserNotFound if the permission receiver does not exist</returns>
     Task<CustomStatusCode> RemoveScheduleManager(int giverUserId, string receiverEmail);
+
+    /// <summary>
+    /// Gets the list of users who are managed by the given user.
+    /// </summary>
+    /// <param name="userId">User id of the requesting user.</param>
+    /// <returns></returns>
+    Task<IEnumerable<UserPublicInfo>> GetManagedUsers(int userId);
 }
---
+++ DAL_Tests/ScheduleManagersServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using DAL.DbAccess;
+using DAL.Models;
+using DAL.Services.Implementations;
+using DAL.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL_Tests;
+
+[Collection("sequential")]
+[TestCaseOrderer("DAL_Tests.PriorityOrderer", "DAL_Tests")]
+public class ScheduleManagersServiceTests
+{
+    private readonly IConfiguration _configuration;
+    private readonly IScheduleManagersService _scheduleManagersService;
+
+    private static User _managedUser = new()
+    {
+        UserId = 2,
+        Email = "aaa"
+    };
+    
+    private static User _managerUser = new()
+    {
+        UserId = 53,
+        Email = "bbb"
+    };
+    
+    public ScheduleManagersServiceTests()
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        _scheduleManagersService = new ScheduleManagersService(new GenericDbAccess(_configuration));
+    }
+    
+    [Fact, TestPriority(1)]
+    public void GetScheduleManagersById_ShouldReturnEmptyList_WhenNoScheduleManagers()
+    {
+        // Arrange
+        
+        // Act
+        var (statusCode, scheduleManagers) = _scheduleManagersService.GetScheduleManagers(userId: _managedUser.UserId).Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.Ok, statusCode);
+        Assert.Empty(scheduleManagers);
+    }
+    
+    [Fact, TestPriority(1)]
+    public void GetScheduleManagersByEmail_ShouldReturnEmptyList_WhenNoScheduleManagers()
+    {
+        // Arrange
+        
+        // Act
+        var (statusCode, scheduleManagers) = _scheduleManagersService.GetScheduleManagers(userEmail: _managedUser.Email).Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.Ok, statusCode);
+        Assert.Empty(scheduleManagers);
+    }
+
+    [Fact, TestPriority(1)]
+    public void GetScheduleManagersByEmailAndId_ShouldFail_ReturnTooManyValuesProvided()
+    {
+        // Arrange
+        
+        // Act
+        var (statusCode, scheduleManagers) = _scheduleManagersService.GetScheduleManagers(userEmail: _managedUser.Email, userId: _managedUser.UserId).Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.TooManyValuesProvided, statusCode);
+        Assert.Empty(scheduleManagers);
+    }
+    
+    [Fact, TestPriority(1)]
+    public void GetScheduleManagersByNoValues_ShouldFail_ReturnParameterMustNotBeNullOrEmpty()
+    {
+        // Arrange
+        
+        // Act
+        var (statusCode, scheduleManagers) = _scheduleManagersService.GetScheduleManagers().Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.ParameterMustNotBeNullOrEmpty, statusCode);
+        Assert.Empty(scheduleManagers);
+    }
+    
+    [Fact, TestPriority(1)]
+    public void GetManagedUsers_ShouldReturnEmptyList_WhenNoManagedUsers()
+    {
+        // Arrange
+        
+        // Act
+        var managedUsers = _scheduleManagersService.GetManagedUsers(_managerUser.UserId).Result;
+        
+        // Assert
+        Assert.Empty(managedUsers);
+    }
+    
+    [Fact, TestPriority(2)]
+    public void AddScheduleManager_ShouldReturnOk_WhenScheduleManagerAdded()
+    {
+        // Arrange
+        
+        // Act
+        var statusCode = _scheduleManagersService.AddScheduleManager(_managerUser.UserId, _managedUser.Email).Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.Ok, statusCode);
+    }
+    
+    [Fact, TestPriority(100)]
+    public void RemoveScheduleManagerShouldWork()
+    {
+        // Arrange
+        
+        // Act
+        var statusCode = _scheduleManagersService.RemoveScheduleManager(_managerUser.UserId, _managedUser.Email).Result;
+        
+        // Assert
+        Assert.Equal(CustomStatusCode.Ok, statusCode);
+    }
+}
