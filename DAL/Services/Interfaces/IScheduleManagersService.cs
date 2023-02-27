d37c861a43c3469c4ae0191df89a66b302302c36
Yuval Uner <yuvaluner@gmail.com>
2023-02-27 16:27:53 +0200

    [CMP-32] Added methods for adding, getting and removing users who can manage other's schedules.

---
+++ DAL/Services/Interfaces/IScheduleManagersService.cs
@@ -0,0 +1,11 @@
+﻿using DAL.DbAccess;
+using DAL.Models;
+
+namespace DAL.Services.Interfaces;
+
+public interface IScheduleManagersService
+{
+    Task<IEnumerable<User>> GetScheduleManagers(string userEmail);
+    Task<CustomStatusCode> AddScheduleManager(int giverUserId, string receiverEmail);
+    Task<CustomStatusCode> RemoveScheduleManager(int giverUserId, string receiverEmail);
+}
