dfc9f1b0bd5dfc92a2d5b8b4a734dddfae60050e
Yuval Uner <yuvaluner@gmail.com>
2023-03-03 17:13:10 +0200

    [CMP-44] Added stored procedures and service for publishing events.

---
+++ DAL/Services/Interfaces/IPublishingService.cs
@@ -0,0 +1,9 @@
+﻿using DAL.DbAccess;
+
+namespace DAL.Services.Interfaces;
+
+public interface IPublishingService
+{
+    Task<CustomStatusCode> PublishEvent(Guid? eventGuid, int? publisherId);
+    Task<CustomStatusCode> UnpublishEvent(Guid? eventGuid);
+}
