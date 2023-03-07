a1d663285ffe83781641e995c71d220965d75774
Yuval Uner <yuvaluner@gmail.com>
2023-03-07 15:49:56 +0200

    [CMP-42] Added CRUD methods for letting users sign up for notifications from campaigns.

--- DAL/Services/Implementations/PublicBoardService.cs
+++ DAL/Services/Implementations/PublicBoardService.cs
@@ -1,4 +1,5 @@
-﻿using DAL.DbAccess;
+﻿using System.Data;
+using DAL.DbAccess;
 using DAL.Models;
 using DAL.Services.Interfaces;
 using Dapper;
@@ -54,5 +55,64 @@ public class PublicBoardService: IPublicBoardService
     {
         return await _dbAccess.GetData<AnnouncementWithPublisherDetails, AnnouncementsSearchParams>
             (StoredProcedureNames.SearchPublishedAnnouncements, searchParams);
+    }
+
+    public async Task<CustomStatusCode> AddNotificationSettings(NotificationUponPublishSettings settings)
+    {
+        var param = new DynamicParameters(new
+        {
+            settings.UserId,
+            settings.CampaignGuid,
+            settings.ViaSms,
+            settings.ViaEmail
+        });
+        
+        param.Add("returnVal", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+        
+        await _dbAccess.ModifyData(StoredProcedureNames.ModifyUserNotificationSettingsOnPublish, param);
+        
+        return  param.Get<CustomStatusCode>("returnVal");
+    }
+    
+    public async Task<CustomStatusCode> RemoveNotificationSettings(NotificationUponPublishSettings settings)
+    {
+        // A wrapper method for AddNotificationSettings, which is used to update the settings.
+        // This is due to the stored procedure being used to add, update or delete the settings.
+        // Made purely for the sake of clarity.
+        settings.ViaEmail = false;
+        settings.ViaSms = false;
+        
+        return await AddNotificationSettings(settings);
+    }
+
+    
+    public async Task<CustomStatusCode> UpdateNotificationSettings(NotificationUponPublishSettings settings)
+    {
+        // A wrapper method for AddNotificationSettings, which is used to update the settings.
+        // This is due to the stored procedure being used to add, update or delete the settings.
+        // Made purely for the sake of clarity.
+        return await AddNotificationSettings(settings);
+    }
+    
+    public async Task<IEnumerable<NotificationUponPublishSettingsForUser>> GetNotificationSettingsForUser(int userId)
+    {
+        var param = new DynamicParameters(new
+        {
+            userId
+        });
+        
+        return await _dbAccess.GetData<NotificationUponPublishSettingsForUser, DynamicParameters>
+            (StoredProcedureNames.GetUserNotificationSettingsOnPublishForUser, param);
+    }
+    
+    public async Task<IEnumerable<NotificationUponPublishSettingsForCampaign>> GetNotificationSettingsForCampaign(Guid campaignGuid)
+    {
+        var param = new DynamicParameters(new
+        {
+            campaignGuid
+        });
+        
+        return await _dbAccess.GetData<NotificationUponPublishSettingsForCampaign, DynamicParameters>
+            (StoredProcedureNames.GetUserNotificationSettingsOnPublishForCampaign, param);
     }
 }
