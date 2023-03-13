5cbea80b6a96a401dac79cb7eca78a682417be9b
Yuval Uner <yuvaluner@gmail.com>
2023-03-13 19:03:44 +0200

    [CMP-261] Documented some more files. No end can be seen in sight just yet.

--- RestAPIServices/SmsMessageSendingService.cs
+++ RestAPIServices/SmsMessageSendingService.cs
@@ -19,7 +19,6 @@ public enum CallStatus
     Failed = 0
 }
 
-
 /// <summary>
 /// A class that transforms a phone number to a specific format.
 /// Uses the builder pattern to allow for easy chaining of transformations.
@@ -27,28 +26,49 @@ public enum CallStatus
 internal class PhoneNumberTransformer
 {
     private delegate string PhoneNumberTransformerDelegate(string? phoneNumber);
+
     private readonly List<PhoneNumberTransformerDelegate> _transformers = new();
-    
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="PhoneNumberTransformer"/> class.
+    /// </summary>
+    /// <returns>The created instance.</returns>
     public static PhoneNumberTransformer Create()
     {
         return new PhoneNumberTransformer();
     }
-    
+
+    /// <summary>
+    /// Adds a transformer to the list of transformers.
+    /// </summary>
+    /// <param name="transformer">A <see cref="PhoneNumberTransformerDelegate"/></param>
+    /// <returns>The object itself.</returns>
     private PhoneNumberTransformer AddTransformer(PhoneNumberTransformerDelegate transformer)
     {
         _transformers.Add(transformer);
         return this;
     }
-    
+
+    /// <summary>
+    /// Activates all the transformers in order and returns the transformed phone number.
+    /// If the phone number is null, returns an empty string.
+    /// </summary>
+    /// <param name="phoneNumber"></param>
+    /// <returns></returns>
     public string Transform(string? phoneNumber)
     {
         foreach (var transformer in _transformers)
         {
             phoneNumber = transformer(phoneNumber);
         }
-        return phoneNumber;
+
+        return phoneNumber ?? "";
     }
-    
+
+    /// <summary>
+    /// Cleans the phone number from spaces and dashes.
+    /// </summary>
+    /// <returns>The object itself.</returns>
     public PhoneNumberTransformer CleanPhoneNumber()
     {
         string PhoneNumber(string? phoneNumber)
@@ -60,19 +80,24 @@ internal class PhoneNumberTransformer
         return AddTransformer(PhoneNumber);
     }
 
+    /// <summary>
+    /// Adds the country code to the phone number.
+    /// </summary>
+    /// <param name="code">Which code, defined in <see cref="CountryCodes"/> to add</param>
+    /// <param name="keepZero">Whether to keep the initial 0 in the phone number or not. Some providers need it, and some don't.</param>
+    /// <returns>The object itself.</returns>
     public PhoneNumberTransformer AddCountryCode(CountryCodes code, bool keepZero)
     {
         string CountryCode(string? phoneNumber)
         {
             if (phoneNumber == null) return "";
             // Depending on the SMS service, some want to keep the initial 0 in numbers, and some do not.
-            if (keepZero) return (int) code + phoneNumber;
-            return (int) code + phoneNumber.Substring(1);
+            if (keepZero) return (int)code + phoneNumber;
+            return (int)code + phoneNumber.Substring(1);
         }
 
         return AddTransformer(CountryCode);
     }
-
 }
 
 public class SmsMessageSendingService : ISmsMessageSendingService
@@ -80,7 +105,7 @@ public class SmsMessageSendingService : ISmsMessageSendingService
     private readonly IConfiguration _configuration;
     private readonly ILogger<SmsMessageSendingService> _logger;
 
-    private static List<int> TelesignNotFailedStatusCodes = new() { 290, 291, 292, 295, 251, 250, 200, 201, 203 };
+    private static List<int> _telesignNotFailedStatusCodes = new() { 290, 291, 292, 295, 251, 250, 200, 201, 203 };
 
     public SmsMessageSendingService(IConfiguration configuration, ILogger<SmsMessageSendingService> logger)
     {
@@ -88,10 +113,13 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         _logger = logger;
     }
 
-    public async Task<int> SendSmsMessageAsync(string phoneNumber, string message)
+    public async Task<int> SendSmsMessageAsync(string? phoneNumber, string? message)
     {
         try
         {
+            // If either the phone number or the message is null, return -1 - this is an error.
+            if (string.IsNullOrWhiteSpace(phoneNumber) || string.IsNullOrWhiteSpace(message)) return -1;
+
             MessagingClient messagingClient = new MessagingClient(
                 _configuration["SmsConfiguration:Telesign:CustomerId"],
                 _configuration["SmsConfiguration:Telesign:ApiKey"]);
@@ -113,7 +141,6 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
         await SendSmsMessageAsync(phoneNumber, message);
-
     }
 
     public async Task SendPhoneVerificationCodeAsync(string? phoneNumber, string code, CountryCodes countryCode)
@@ -156,7 +183,7 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
         var res = await SendSmsMessageAsync(phoneNumber, message);
-        return TelesignNotFailedStatusCodes.Contains(res) ? CallStatus.Success : CallStatus.Failed;
+        return _telesignNotFailedStatusCodes.Contains(res) ? CallStatus.Success : CallStatus.Failed;
     }
 
     public async Task SendAddedEventParticipationSmsAsync(string? eventName,
@@ -181,11 +208,12 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         {
             message = $"You were added to the event {eventName} at {eventLocation}";
         }
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 
     public async Task SendEventCreatedForUserSmsAsync(string? eventName,
-        string? eventLocation, DateTime? startTime, DateTime? endTime, string creatorName,
+        string? eventLocation, DateTime? startTime, DateTime? endTime, string? creatorName,
         string? phoneNumber, CountryCodes countryCode)
     {
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
@@ -198,18 +226,22 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         }
         else if (startTime != null)
         {
-            message = $"The event {eventName} at {eventLocation} from {startTime} was created by {creatorName} and added" +
-                      $" to your schedule";
+            message =
+                $"The event {eventName} at {eventLocation} from {startTime} was created by {creatorName} and added" +
+                $" to your schedule";
         }
         else if (endTime != null)
         {
-            message = $"The event {eventName} at {eventLocation} until {endTime} was created by {creatorName} and added" +
-                      $" to your schedule";
+            message =
+                $"The event {eventName} at {eventLocation} until {endTime} was created by {creatorName} and added" +
+                $" to your schedule";
         }
         else
         {
-            message = $"The event {eventName} at {eventLocation} was created by {creatorName} and added to your schedule";
+            message =
+                $"The event {eventName} at {eventLocation} was created by {creatorName} and added to your schedule";
         }
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 
@@ -218,11 +250,12 @@ public class SmsMessageSendingService : ISmsMessageSendingService
     {
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
-        
+
         string message;
         if (startTime != null && endTime != null)
         {
-            message = $"{senderName}: \n The event {eventName} at {eventLocation} from {startTime} to {endTime} was deleted";
+            message =
+                $"{senderName}: \n The event {eventName} at {eventLocation} from {startTime} to {endTime} was deleted";
         }
         else if (startTime != null)
         {
@@ -236,6 +269,7 @@ public class SmsMessageSendingService : ISmsMessageSendingService
         {
             message = $"{senderName}: \n The event {eventName} at {eventLocation} was deleted";
         }
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 
@@ -244,20 +278,23 @@ public class SmsMessageSendingService : ISmsMessageSendingService
     {
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
-        
+
         string message = $"{senderName}'s event {eventName} updated \n";
         if (eventLocation != null)
         {
             message += $"Location moved to: {eventLocation} \n";
         }
+
         if (startTime != null)
         {
             message += $"Start time changed to: {startTime} \n";
         }
+
         if (endTime != null)
         {
             message += $"End time changed to: {endTime} \n";
         }
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 
@@ -266,20 +303,23 @@ public class SmsMessageSendingService : ISmsMessageSendingService
     {
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
-        
+
         string message = $"{senderName}'s event {eventName} published \n";
         if (eventLocation != null)
         {
             message += $"Location: {eventLocation} \n";
         }
+
         if (startTime != null)
         {
             message += $"Start time: {startTime} \n";
         }
+
         if (endTime != null)
         {
             message += $"End time: {endTime} \n";
         }
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 
@@ -288,9 +328,9 @@ public class SmsMessageSendingService : ISmsMessageSendingService
     {
         phoneNumber = PhoneNumberTransformer.Create().CleanPhoneNumber().AddCountryCode(countryCode, true)
             .Transform(phoneNumber);
-        
+
         string message = $"""{senderName}'s new announcement "{announcementTitle}" published""";
-        
+
         await SendSmsMessageAsync(phoneNumber, message);
     }
 }
