be8b5b541ec516f744d4c68fb0e02137389963a9
Yuval Uner <yuvaluner@gmail.com>
2023-05-16 16:59:01 +0300

    [CMP-297] Added integration of server .net code with python process for using pre-trained NLP models. Additionally, added the python script nlp_runner.py to the project, and made everything run within the project's directory context.

---
+++ API/Controllers/CampaignAdvisorController.cs
@@ -0,0 +1,28 @@
+﻿using API.ExternalProcesses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class CampaignAdvisorController : Controller
+{
+    private readonly IPythonMlRunner _pythonMlRunner;
+    
+    public CampaignAdvisorController(IPythonMlRunner pythonMlRunner)
+    {
+        _pythonMlRunner = pythonMlRunner;
+    }
+    
+    [HttpPost("analyze")]
+    public async Task<IActionResult> Analyze([FromBody] string opponentName)
+    {
+        var articles = new string[] {"article1", "article2"};
+        var tweets = new string[] {"tweet1", "tweet2"};
+        var classifiedTexts = await _pythonMlRunner.RunPythonScript(articles, tweets);
+        return Ok(classifiedTexts);
+    }
+    
+}
