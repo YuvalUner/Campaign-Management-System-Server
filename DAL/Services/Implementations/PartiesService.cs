f5bd2f98b735aebb262693b75fd9bce9c856b05b
Yuval Uner <yuvaluner@gmail.com>
2023-06-02 19:30:58 +0300

    [CMP-303] Added stored procedures, services and API endpoints for all CRUD operations relating to registering parties for election day.

---
+++ DAL/Services/Implementations/PartiesService.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using DAL.DbAccess;
+using DAL.Models;
+using DAL.Services.Interfaces;
+using Dapper;
+
+namespace DAL.Services.Implementations;
+
+
+public class PartiesService : IPartiesService
+{
+    private readonly IGenericDbAccess _dbAccess;
+    
+    public PartiesService(IGenericDbAccess dbAccess)
+    {
+        _dbAccess = dbAccess;
+    }
+
+    public async Task<CustomStatusCode> AddParty(Party party, Guid campaignGuid)
+    {
+        var param = new DynamicParameters(new
+        {
+            party.PartyLetter,
+            party.PartyName,
+            campaignGuid
+        });
+        
+        param.Add("returnVal", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+        
+        await _dbAccess.ModifyData(StoredProcedureNames.AddParty, param);
+        
+        return param.Get<CustomStatusCode>("returnVal");
+    }
+    
+    public async Task<CustomStatusCode> UpdateParty(Party party, Guid campaignGuid)
+    {
+        var param = new DynamicParameters(new
+        {
+            party.PartyId,
+            party.PartyLetter,
+            party.PartyName,
+            campaignGuid
+        });
+        
+        param.Add("returnVal", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+        
+        await _dbAccess.ModifyData(StoredProcedureNames.UpdateParty, param);
+        
+        return param.Get<CustomStatusCode>("returnVal");
+    }
+    
+    public async Task<CustomStatusCode> DeleteParty(int partyId, Guid campaignGuid)
+    {
+        var param = new DynamicParameters(new
+        {
+            partyId,
+            campaignGuid
+        });
+        
+        param.Add("returnVal", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+        
+        await _dbAccess.ModifyData(StoredProcedureNames.DeleteParty, param);
+        
+        return param.Get<CustomStatusCode>("returnVal");
+    }
+    
+    public async Task<IEnumerable<Party>> GetParties(Guid campaignGuid)
+    {
+        var param = new DynamicParameters(new
+        {
+            campaignGuid
+        });
+        
+        return await _dbAccess.GetData<Party, DynamicParameters>(StoredProcedureNames.GetPartiesForCampaign, param);
+    }
+}
